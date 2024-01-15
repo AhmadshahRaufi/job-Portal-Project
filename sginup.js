49efe4a997602123f190436f25ad9730d2f18bf4
ahmad shah raufi <131220146+AhmadshahRaufi@users.noreply.github.com>
2024-01-15 19:20:42 +0500

    job portal
    This project is a web-based job portal designed to connect job seekers with potential employers. It provides a platform for users to search for job opportunities, submit applications, and for employers to post job listings and manage

---
+++ sginup.js
@@ -0,0 +1,44 @@
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('d1');
+  
+    if (window.scrollY > 100) {
+        navbar.classList.add('scroll');
+    } else {
+        navbar.classList.remove('scroll');
+    }
+  });
+  const btn = document.querySelector('.sidebarBtn');
+  const csidebar= document.querySelector('.sidebar-container');
+
+  btn.addEventListener('click',()=>{
+     csidebar.style.visibility='visible'; 
+ 
+    console.log("sdsdsd")
+    
+    } 
+  )
+  const closeSidebarbtn= document.querySelector(".sidebrclosebtn");
+  closeSidebarbtn.addEventListener("click",()=>{
+    csidebar.style.visibility='hidden'; 
+  })
+  const element = document.getElementById('myElement'); // Replace with the actual element ID
+const hideElement = () => {
+  if (window.innerWidth > 700) {
+    csidebar.style.visibility = 'hidden';
+  }
+};
+
+window.addEventListener('resize', hideElement); // Recheck on resize events
+hideElement(); // Initial check on page load
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const showPasswordButton = document.querySelector('.password-visibility');
+
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      showPasswordButton.textContent = 'Hide Password';
+    } else {
+      passwordInput.type = 'password';
+      showPasswordButton.textContent = 'Show Password';
+    }
+  }
